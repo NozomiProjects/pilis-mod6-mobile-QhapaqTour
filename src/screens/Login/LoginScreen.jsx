56c8042ba5fe77c3e5c03753a2782c0556888029
Flores Juan Gabriel <gabriel.flores.tec@gmail.com>
2023-08-29 20:35:13 -0300

    mainScreen button added

--- src/screens/Login/LoginScreen.jsx
+++ src/screens/Login/LoginScreen.jsx
@@ -1,17 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, Button, Alert } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
 export const LoginScreen = () => {
+  const navigation = useNavigation();
+
+  const goToMainScreen = () => {
+    navigation.navigate('Main'); // Navegar al MainScreen
+  };
+
   return (
     <View style={styles.container}>
       <Text>Login Screen</Text>
+      <Button title="Ir a MainScreen" onPress={goToMainScreen} />
       <StatusBar style="auto" />
     </View>
   );
 }
-
-
 
 const styles = StyleSheet.create({
   container: {
@@ -21,3 +27,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+
