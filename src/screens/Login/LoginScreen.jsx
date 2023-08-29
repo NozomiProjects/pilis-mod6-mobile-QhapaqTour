f537701bb549e827c7e19c2f75649b702f7cc19c
Flores Juan Gabriel <gabriel.flores.tec@gmail.com>
2023-08-29 20:51:04 -0300

    email and password text inputs added

--- src/screens/Login/LoginScreen.jsx
+++ src/screens/Login/LoginScreen.jsx
@@ -1,18 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, Button, Alert } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
 export const LoginScreen = () => {
   const navigation = useNavigation();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
   const goToMainScreen = () => {
     navigation.navigate('Main'); // Navegar al MainScreen
   };
-
   return (
     <View style={styles.container}>
       <Text>Login Screen</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Correo electrónico o cuenta"
+        value={email}
+        onChangeText={setEmail}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Contraseña"
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry
+      />
       <Button title="Ir a MainScreen" onPress={goToMainScreen} />
       <StatusBar style="auto" />
     </View>
@@ -25,6 +39,16 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+
+  input: {
+    width: 250,
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    borderRadius: 5,
+    marginVertical: 10,
+    paddingHorizontal: 10,
   },
 });
 
