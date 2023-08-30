4a526ac331fa4b1c33059b61479185e0825a6046
Flores Juan Gabriel <gabriel.flores.tec@gmail.com>
2023-08-30 17:36:21 -0300

    axios library and login basic logic added

--- src/screens/Login/LoginScreen.jsx
+++ src/screens/Login/LoginScreen.jsx
@@ -1,16 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import React, { useState, useContext } from 'react';
+import { StyleSheet, Text, View, Button, TextInput, Alert } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import { UserContext } from '../../contexts/UserContext';
 
 export const LoginScreen = () => {
   const navigation = useNavigation();
+  const { setCurrentUser } = useContext(UserContext); // Obtenemos el setter del contexto
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
   const goToMainScreen = () => {
     navigation.navigate('Main'); // Navegar al MainScreen
   };
+
+  const handleLogin = async () => {
+    try {
+      const response = await fakeAuthenticationAPI(email, password);
+      if (response.success) {
+        setCurrentUser({ email }); // Actualizamos el contexto con los datos del usuario
+        navigation.navigate('Main');
+      } else {
+        Alert.alert('Error', 'Credenciales inválidas. Por favor, intenta nuevamente.');
+      }
+    } catch (error) {
+      Alert.alert('Error', 'Hubo un error en la autenticación. Por favor, intenta nuevamente.');
+    }
+  };
+
   return (
     <View style={styles.container}>
       <Text>Login Screen</Text>
@@ -27,6 +44,7 @@ export const LoginScreen = () => {
         onChangeText={setPassword}
         secureTextEntry
       />
+      <Button title="Iniciar sesión" onPress={handleLogin} />
       <Button title="Ir a MainScreen" onPress={goToMainScreen} />
       <StatusBar style="auto" />
     </View>
