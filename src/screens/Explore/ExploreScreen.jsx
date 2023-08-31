71cf264346326d8441587123bfff0710f55732e3
3215LU <roxanayvbenicio@gmail.com>
2023-08-31 16:37:59 -0300

    explorer, me falta agregar el buscador y el titulo

--- src/screens/Explore/ExploreScreen.jsx
+++ src/screens/Explore/ExploreScreen.jsx
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Text, SafeAreaView, FlatList, Pressable, View, Image } from 'react-native';
+import { styles } from './ExploreScreen.styles.js';
+import { data } from './../../api/data.js';
+import { Entypo } from '@expo/vector-icons';
+import { Ionicons } from '@expo/vector-icons';
+import { COLORS } from '../../utils/theme';
+
+const Location = ({ item }) => (
+  <Pressable>
+    <View style={styles.itemContainer} >
+      <Image source={item.Images} style={styles.itemImage} />
+      <View style={styles.itemTextContainer}>
+        <Text style={styles.itemLugar}>{item.Lugar}</Text>
+        <Text style={styles.itemLocalidad}><Ionicons name="md-location-sharp" size={24} color={COLORS.primary} />{item.Localidad}</Text>
+        <View style={styles.itemRowContainer}>
+          <Text style={ styles.itemTiempo}>{item.Tiempo}hs.</Text>
+          <Text style={styles.itemPrecio }>{item.Precio} ARS</Text>
+        </View>
+        <View style={styles.itemRowContain}>
+          <Image source={item.ImgGuia} style={styles.itemImgGuia} />
+          <Text style={styles.itemGuia}>{item.Guia}</Text>         
+        </View>        
+        <Text style={styles.itemCalificacion}>{item.Calificacion}<Entypo name="star" size={14} color="black" /></Text>
+      </View>
+    </View>
+  </Pressable>
+);
 
 export const ExploreScreen = () => {
   return (
-    <View style={styles.container}>
-      <Text>Explore Screen</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      
+      <FlatList
+        data={data}
+        renderItem={Location}
+        keyExtractor={item => item.id}
+        style={styles.itemList}
+      />
+    </SafeAreaView>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/screens/Explore/ExploreScreen.styles.js
@@ -0,0 +1,103 @@
+import { StyleSheet, Dimensions } from 'react-native';
+import { COLORS } from '../../utils/theme';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F1F1F1',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  itemContainer: {
+    flexDirection: 'row', // Alineamos elementos en fila
+    width: Dimensions.get('screen').width - 20,
+    borderRadius: 20,
+    overflow: 'hidden',
+    marginBottom: 10,
+    backgroundColor: COLORS.white,
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    shadowRadius: 2,
+  },  
+  itemRowContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between', // Alinea los elementos con espacio entre ellos
+    marginTop: 2,
+  },
+  itemRowContain: {
+    flexDirection: 'row',
+    alignItems: 'center',    
+    marginTop: 2,
+  },
+  itemRowRight: {
+    flexDirection: 'row', // Alinea los elementos en fila
+    
+    alignItems: 'center',
+  },
+  itemList: {
+    padding: 10,
+  },
+  itemImage: {
+    marginTop: '4%',
+    marginLeft: '4%',
+    height: 80,
+    width: '40%',
+    resizeMode: 'cover',
+    borderRadius: 5,
+  },
+  itemTextContainer: {
+    marginLeft: '4%', // Espacio entre imagen y texto
+    width: '46%', // Ajustamos el ancho del contenedor de texto
+  },
+  itemLugar: {
+    fontWeight: 'bold',
+    marginTop: 10,
+    marginBottom: 1,
+    marginHorizontal: 0,
+    fontSize:13,
+  },
+  itemLocalidad: {
+    marginBottom: 1,
+    marginHorizontal: 0,
+    fontSize:11,
+  },
+  itemTiempo: {
+    marginBottom: 1,
+    marginHorizontal: 0,
+    fontSize: 10,
+    marginRight: 20
+  },
+  itemPrecio: {
+    marginBottom: 1,
+    marginHorizontal: 0,    
+    fontSize: 12,
+    textAlign: 'right', // Alinea el texto a la derecha
+  },
+  itemGuia: {
+    marginBottom: 1,
+    marginHorizontal: 0,
+    fontSize:12,
+    marginRight:7
+  },
+  
+  itemCalificacion: {
+    marginBottom: 1,
+    marginHorizontal: 0,
+    marginRight:0,
+    fontSize:10,
+    textAlign: 'right',
+  },  
+  
+  itemGuia: {
+    marginBottom: 1,
+    marginHorizontal: 0,
+    fontSize:12,    
+  },
+  itemImgGuia: {
+    width: 19,
+    height: 17,
+    borderRadius: 20,    
+    marginRight:4,
+  },
+});
